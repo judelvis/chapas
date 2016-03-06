4b49b3be6bd7cec4f21fc088e6598ea34dbb278a
judelvis <jud.prog@gmail.com>
2016-03-06 18:07:51 -0430

    cambio en base de datos

--- chapas.sql
+++ chapas.sql
@@ -1,45 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-03-2016 a las 16:13:37
--- Versión del servidor: 5.5.32
--- Versión de PHP: 5.4.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de datos: `chapas`
---
-CREATE DATABASE IF NOT EXISTS `chapas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci;
-USE `chapas`;
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.5.47-0ubuntu0.14.04.1 - (Ubuntu)
+-- SO del servidor:              debian-linux-gnu
+-- HeidiSQL Versión:             9.3.0.4984
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `t_categorias`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Volcando estructura para tabla chapas.t_categorias
 CREATE TABLE IF NOT EXISTS `t_categorias` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(80) COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
+-- Volcando datos para la tabla chapas.t_categorias: 0 rows
+/*!40000 ALTER TABLE `t_categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_categorias` ENABLE KEYS */;
 
---
--- Estructura de tabla para la tabla `t_cupon`
---
 
+-- Volcando estructura para tabla chapas.t_cupon
 CREATE TABLE IF NOT EXISTS `t_cupon` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `id_negocio` tinyint(4) NOT NULL,
@@ -49,28 +32,30 @@ CREATE TABLE IF NOT EXISTS `t_cupon` (
   `monto` float(9,2) NOT NULL,
   `fecha_pub` date NOT NULL,
   `fecha_exp` date NOT NULL,
+  `destacado` int(11) NOT NULL DEFAULT '0',
+  `imagen` text COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
+-- Volcando datos para la tabla chapas.t_cupon: 0 rows
+/*!40000 ALTER TABLE `t_cupon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_cupon` ENABLE KEYS */;
 
---
--- Estructura de tabla para la tabla `t_negocios`
---
 
+-- Volcando estructura para tabla chapas.t_negocios
 CREATE TABLE IF NOT EXISTS `t_negocios` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `id_usuario` int(11) NOT NULL,
   `nombre` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
+-- Volcando datos para la tabla chapas.t_negocios: 0 rows
+/*!40000 ALTER TABLE `t_negocios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_negocios` ENABLE KEYS */;
 
---
--- Estructura de tabla para la tabla `t_usuarios`
---
 
+-- Volcando estructura para tabla chapas.t_usuarios
 CREATE TABLE IF NOT EXISTS `t_usuarios` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `nombres` varchar(40) COLLATE utf8mb4_spanish_ci NOT NULL,
@@ -82,8 +67,11 @@ CREATE TABLE IF NOT EXISTS `t_usuarios` (
   `nivel` tinyint(2) NOT NULL COMMENT '1:Admin | 2:Socio | 3:Cliente',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+-- Volcando datos para la tabla chapas.t_usuarios: 0 rows
+/*!40000 ALTER TABLE `t_usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_usuarios` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
